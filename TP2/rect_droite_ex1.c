e813946f3c956cac11086085347002eda2b96b1c
Yassin-Latif <yassin.latif@um6p.ma>
2021-01-06 18:36:16 +0100

    Update

---
+++ TP2/rect_droite_ex1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+double f(int t)
+{
+	return t;
+}
+
+double rectangle_droite( double a, double b, int n)
+{
+	int i;
+	double h = (b - a) / n;
+	float s = 0;
+	for (i = 0; i <= n; i++)
+	{
+		s+= f(a + i * h);
+	}
+	return h*s;
+}
+
+int main()
+{
+	double a, b;
+	int n;
+	printf("La valeur de a est : ");
+	scanf("%lf", &a);
+	printf("La valeur de b est : ");
+	scanf("%lf", &b);	
+	printf("La valeur de n est : ");
+	scanf("%d", &n);
+	double x;
+	x = rectangle_droite(a,b, n);
+	printf("Result is : %lf", x);
+}
